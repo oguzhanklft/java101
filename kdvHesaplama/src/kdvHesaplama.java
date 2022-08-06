a34e41fb09321c4020d184336e7d6238d117517e
Oguzhan Kalafat <oguzhankalafattt@hotmail.com>
2022-08-07 02:08:08 +0300

    kdvHesaplama changed

--- kdvHesaplama/src/kdvHesaplama.java
+++ kdvHesaplama/src/kdvHesaplama.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class kdvHesaplama {
+    public static void main(String[] args) {
+
+        // Tanımlar Yapıldı
+        double kdvsizTutar,kdvYuzdesi,kdvTutari,kdvliTutar;
+
+        // Kdvsiz Tutar Hesaplandı
+        Scanner tutar = new Scanner(System.in);
+        System.out.print("Tutarı Giriniz: ");
+        kdvsizTutar = tutar.nextDouble();
+
+        // Ürünün Tutarına Bağlı olarak Uygulanan KDV yüzdesi
+        kdvYuzdesi = (kdvsizTutar <= 1000) ? 0.18 : 0.08;
+        kdvTutari  = kdvsizTutar * kdvYuzdesi;
+        kdvliTutar = kdvsizTutar + kdvTutari;
+
+        System.out.println("Ürünün KDVsiz Tutarı: " +kdvsizTutar);
+        System.out.println("Ürünün KDV Yüzdesi: " +kdvYuzdesi);
+        System.out.println("Ürünün KDV Tutarı: " +(kdvliTutar - kdvsizTutar));
+        System.out.println("Ürünün KDVli Tutarı: " +kdvliTutar);
+    }
 }
