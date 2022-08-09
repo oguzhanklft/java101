3f3313c9ec26a626167ac426c7508a392b72737e
Oguzhan Kalafat <oguzhankalafattt@hotmail.com>
2022-08-09 15:39:44 +0300

    CinZodyagi created

---
+++ CinZodyagi/src/CinZodyagi.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class CinZodyagi {
+    public static void main(String[] args) {
+        int birthDate,chineseZodiac;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Doğum Yılınız: ");
+        birthDate = inp.nextInt();
+
+        chineseZodiac = birthDate%12;
+
+        switch(chineseZodiac){
+            case 0:
+                System.out.println("Maymun");
+                break;
+            case 1:
+                System.out.println("Horoz");
+                break;
+            case 2:
+                System.out.println("Köpek");
+                break;
+            case 3:
+                System.out.println("Domuz");
+                break;
+            case 4:
+                System.out.println("Fare");
+                break;
+            case 5:
+                System.out.println("Öküz");
+                break;
+            case 6:
+                System.out.println("Kaplan");
+                break;
+            case 7:
+                System.out.println("Tavşan");
+                break;
+            case 8:
+                System.out.println("Ejderha");
+                break;
+            case 9:
+                System.out.println("Yılan");
+                break;
+            case 10:
+                System.out.println("At");
+                break;
+            case 11:
+                System.out.println("Koyun");
+                break;
+        }
+    }
+    }
