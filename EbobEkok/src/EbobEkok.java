0c5f1be1de13e3f887ebb1bf8e83587523e784d6
Oguzhan Kalafat <oguzhankalafattt@hotmail.com>
2022-08-11 01:19:28 +0300

    EbobEkok changed

--- EbobEkok/src/EbobEkok.java
+++ EbobEkok/src/EbobEkok.java
@@ -2,30 +2,44 @@ import java.util.Scanner;
 
 public class EbobEkok {
     public static void main(String[] args) {
-        int firstNumber, secondNumber, largeNumber;
-        int Ebob = 1, Ekok = 1, divided = 1;
-
-        Scanner inp = new Scanner(System.in);
-        System.out.print("İlk sayıyı giriniz: ");
-        firstNumber = inp.nextInt();
-
-        System.out.print("İkinci sayıyı giriniz: ");
-        secondNumber = inp.nextInt();
-
-        if (firstNumber > secondNumber) {
-            largeNumber = firstNumber;
-        } else {
-            largeNumber = secondNumber;
-        }
-        while (divided < largeNumber) {
-            if (firstNumber % divided == 0 && secondNumber % divided == 0) {
-                Ebob = divided;
-                divided++;
+        Scanner input = new Scanner(System.in);
+        int numberOne, numberTwo;
+        boolean a = false;
+        do {
+            System.out.print("İlk sayıyı giriniz: ");
+            numberOne = input.nextInt();
+            System.out.print("İkinci sayıyı giriniz: ");
+            numberTwo = input.nextInt();
+            if (numberOne <= 0 || numberTwo <= 0) {
+                System.out.println("Pozitif Sayı Giriniz!");
+            } else {
+                a = true;
             }
-
-            Ekok = (firstNumber * secondNumber) / Ebob;
-            System.out.println("En büyük ortak bölen(EBOB): " + Ebob + "ve en küçük ortak kat(EKOK): " + Ekok);
-            break;
+        } while (!a);
+        if (numberTwo < numberOne) {
+            int tempN2 = numberTwo;
+            numberTwo = numberOne;
+            numberOne = tempN2;
         }
+        int i = numberOne;
+        while (i >= 1) {
+            if ((numberOne % i == 0) && (numberTwo % i == 0)) {
+                System.out.println("En büyük ortak bölen(EBOB): " + i);
+                break;
+            } else {
+                i--;
+            }
+        }
+        int k = numberTwo;
+        while (k <= (numberOne * numberTwo)) {
+            if ((k % numberOne == 0) && (k % numberTwo == 0)) {
+                System.out.println("En küçük ortak kat(EKOK): " + k);
+                break;
+            } else {
+                k++;
+            }
+        }
+
+
     }
 }
