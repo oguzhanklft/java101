3d666b03a312a0e2eb09a3c9af37d797c9670558
Oguzhan Kalafat <oguzhankalafattt@hotmail.com>
2022-08-11 15:08:45 +0300

    AsalSayi created

---
+++ AsalSayi/src/AsalSayi.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class AsalSayi {
+    public static void main(String[] args) {
+        int counter = 0,number,i;
+
+        Scanner inp = new Scanner(System.in);
+
+        for(number=2;number<=100; number++){
+            boolean remind=true;
+            for(i=2;i<number; i++) {
+                if (number % i == 0) {
+                    remind = false;
+                    break;
+                }
+            }
+
+            if(remind == true){
+                System.out.print(number +"\n");
+                counter++;
+                }
+            }
+        }
+
+    }
