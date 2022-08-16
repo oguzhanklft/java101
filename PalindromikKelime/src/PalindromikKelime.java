da87487b80cb6032ec43412df100f0cba45d3cb4
Oguzhan Kalafat <oguzhankalafattt@hotmail.com>
2022-08-16 13:32:48 +0300

    PalindromikKelime created

---
+++ PalindromikKelime/src/PalindromikKelime.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class PalindromikKelime {
+    static boolean isPalindrome(String str){
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    static boolean isPalindrome2(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        if (str.equals(reverse))
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        String word;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Kelimeyi giriniz: ");
+        word = scanner.nextLine();
+        scanner.close();
+        System.out.println(isPalindrome(word));
+    }
+}
